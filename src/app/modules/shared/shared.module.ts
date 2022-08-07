f0fe10bcaa006e049d088b4401f0ab02eb06478f
Mikkaiser <mikaelrsimoes19@gmail.com>
2022-08-07 15:35:51 -0300

    add: config to routes

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,10 +1,12 @@
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './../../app-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [NavbarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [NavbarComponent],
 })
 export class SharedModule {}
