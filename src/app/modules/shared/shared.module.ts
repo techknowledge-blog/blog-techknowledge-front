8ff11db4fa32f5eca70601d8741726c36abc3ada
Leticia Dias <contatoleticiadia@gmail.com>
2022-08-07 15:27:17 -0300

    add import/exports HeaderComponent

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, HeaderComponent],
   imports: [CommonModule],
-  exports: [NavbarComponent],
+  exports: [NavbarComponent, HeaderComponent],
 })
 export class SharedModule {}
