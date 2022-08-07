4c9903daf41a51e56b4e92d757dfe0e9ae0fc4db
Mikkaiser <mikaelrsimoes19@gmail.com>
2022-08-07 14:46:20 -0300

    add: shared module

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,10 +2,11 @@ import { HomeRoutingModule } from './home-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, SharedModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
