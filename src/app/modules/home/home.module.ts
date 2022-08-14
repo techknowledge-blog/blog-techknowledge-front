0ec3687f8cb71e2d1d0879e1cf73ea9608341583
Mikkaiser <mikaelrsimoes19@gmail.com>
2022-08-13 21:11:28 -0300

    add: navbar structure

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,11 +1,13 @@
+import { RouterModule } from '@angular/router';
 import { HomeRoutingModule } from './home-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, SharedModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [NavbarComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [NavbarComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
