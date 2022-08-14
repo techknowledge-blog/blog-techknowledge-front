8fccad2188940bad94ac7dbdbb2e8d034ef59285
Mikkaiser <mikaelrsimoes19@gmail.com>
2022-08-14 15:43:27 -0300

    add: cards

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, SharedModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    SharedModule,
+    MatCardModule,
+    MatButtonModule,
+  ],
   exports: [HomeComponent],
 })
 export class HomeModule {}
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { CardComponent } from './components/card/card.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [NavbarComponent, HeaderComponent],
-  imports: [CommonModule, RouterModule],
-  exports: [NavbarComponent, HeaderComponent],
+  declarations: [NavbarComponent, HeaderComponent, CardComponent],
+  imports: [CommonModule, RouterModule, MatCardModule, MatButtonModule],
+  exports: [NavbarComponent, HeaderComponent, CardComponent],
 })
 export class SharedModule {}
