fa44a714b30e9a1b081b297088202f6be694c24b
Mikkaiser <mikaelrsimoes19@gmail.com>
2022-08-15 23:53:37 -0300

    refactor: posts responsibilities

---
+++ src/app/modules/posts/interfaces/post.interface.ts
@@ -0,0 +1,10 @@
+export class IPost {
+  id!: number;
+  title!: string;
+  category!: any;
+  subtitle!: string;
+  content!: string;
+  slug!: string;
+  createdAt!: string;
+  updatedAt!: string;
+}
---
+++ src/app/modules/posts/posts.service.ts
@@ -0,0 +1,17 @@
+import { IPost } from './interfaces/post.interface';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostsService {
+  constructor(private http: HttpClient) {}
+
+  private API_URL = 'http://localhost:3000';
+
+  getFeaturedPosts(): Observable<IPost[]> {
+    return this.http.get<IPost[]>(this.API_URL + '/posts');
+  }
+}
