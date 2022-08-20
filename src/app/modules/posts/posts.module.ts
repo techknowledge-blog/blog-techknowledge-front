05a56ff862c449a881ec50d32987c1cab47e2dcd
Mikkaiser <mikaelrsimoes19@gmail.com>
2022-08-20 18:51:53 -0300

    update: route dinamic id to dinamic slug

--- src/app/modules/posts/posts.module.ts
+++ src/app/modules/posts/posts.module.ts
@@ -1,13 +1,22 @@
+import { PostsRoutingModule } from './posts-routing.module';
 import { PostsCardRenderComponent } from './components/posts-card-render/posts-card-render.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostsComponent } from './posts.component';
 import { SharedModule } from '../shared/shared.module';
 import { PostCardComponent } from './components/post-card/posts-card.component';
+import { SinglePostPageComponent } from './components/single-post-page/single-post-page.component';
+import { PostsPageComponent } from './components/posts-page/posts-page.component';
 
 @NgModule({
-  declarations: [PostsComponent, PostCardComponent, PostsCardRenderComponent],
-  imports: [CommonModule, SharedModule],
+  declarations: [
+    PostsComponent,
+    PostCardComponent,
+    PostsCardRenderComponent,
+    SinglePostPageComponent,
+    PostsPageComponent,
+  ],
+  imports: [CommonModule, SharedModule, PostsRoutingModule],
   exports: [PostsComponent, PostCardComponent, PostsCardRenderComponent],
 })
 export class PostsModule {}
