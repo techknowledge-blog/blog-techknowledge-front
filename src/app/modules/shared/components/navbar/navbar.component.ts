d1438fc04e529d0381ef9fd081e254e7cf19d754
Leticia Dias <contatoleticiadia@gmail.com>
2022-09-17 17:23:54 -0300

    feat: create logic to show content of navbar

--- src/app/modules/shared/components/navbar/navbar.component.ts
+++ src/app/modules/shared/components/navbar/navbar.component.ts
@@ -6,7 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  isBurgerMenu: boolean = true;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.navbar();
+  }
+
+  toggleMenu() {
+    const navbar = document.getElementById('navbar');
+    const btnClose = document.getElementById('btn-close');
+    const btnOpen = document.getElementById('btn-mobile');
+
+    if (!this.isBurgerMenu) {
+      btnClose?.classList.add('close');
+      btnOpen?.classList.add('icon-hamgurber-remove');
+
+      btnClose?.addEventListener('click', () => {
+        btnClose?.classList.remove('close');
+        navbar?.classList.remove('active');
+        btnOpen?.classList.remove('icon-hamgurber-remove');
+      })
+    }
+
+    navbar?.classList.add('active');
+  }
+
+  navbar() {
+    const btnMobile = document.getElementById('btn-mobile');
+
+    btnMobile?.addEventListener('click', this.toggleMenu);
+  }
 }
