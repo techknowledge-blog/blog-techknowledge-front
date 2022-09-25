0d5bb67b1884c1b2ab65eb4280058bed2d57eb95
Mikkaiser <mikaelrsimoes19@gmail.com>
2022-09-25 17:00:16 -0300

    add: header updates

--- src/app/modules/shared/components/navbar/navbar.component.ts
+++ src/app/modules/shared/components/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  showMenu: boolean = true;
+  showMenu: boolean = false;
 
   constructor() {}
 
